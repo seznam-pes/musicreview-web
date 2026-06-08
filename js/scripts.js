f05ee60d08942f1e3443665e4c2f3af825232e2f
seznam pes <61787burda@student.sstebrno.cz>
2026-06-08 08:04:56 +0200

    Implement dark mode and light mode functions
    Added dark mode and light mode functions for theme switching.

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,19 @@
 function Submited() {
     alert("Vaše album bylo zasláno a brzy se na něj podívám!");
 }
+
+bool dark = false;
+
+function DarkMode() {
+if (dark == false) {
+    
+}
+    else {}
+}
+
+function LightMode() {
+if (dark == true) {
+    
+}
+    else {}
+}
