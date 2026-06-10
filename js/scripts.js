072b726eecc79bc5222231f3de8d1fde09d62a78
seznam pes <61787burda@student.sstebrno.cz>
2026-06-10 11:23:20 +0200

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -6,6 +6,7 @@ let dark = false;
 const headers = document.getElementsByTagName("header");
 const links = document.getElementsByTagName("a");
 const cards = document.getElementsByClassName("horiz-card");
+const buttons = document.getElementsbyTagName("button");
 
 function DarkMode() {
     if (dark === false) {
@@ -19,6 +20,13 @@ function DarkMode() {
             cards[i].style.color = "#f9f4f5";
             cards[i].style.backgroundColor = "#10011a";
         }
+
+        for (let i = 0; i < buttons.length; i++) {
+            buttons[i].style.backgroundColor = "#092607";
+        }
+
+        document.getElementByTagName("menu").style.backgroundColor = "#10011a";
+        document.getElementByTagName("menu").style.color = "#f9f4f5";
     }
     else {
         dark = false;
@@ -31,5 +39,12 @@ function DarkMode() {
             cards[i].style.backgroundColor = "#f9f4f5";
             cards[i].style.color = "#10011a";
         }
+
+        for (let i = 0; i < buttons.length; i++) {
+            buttons[i].style.backgroundColor = "#effeee";
+        }
+
+        document.getElementByTagName("menu").style.backgroundColor = "#f9f4f5";
+        document.getElementByTagName("menu").style.color = "#10011a";
     }
 }
